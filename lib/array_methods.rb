098b6c5bdf35d4d4bea39d9e00d516944dfd9705
Kate Summerill <kaitlynsummerill@gmail.com>
2019-08-16 01:05:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,22 +3,22 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.sort
 end
 
 def using_reverse(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_first(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_last(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_size(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
